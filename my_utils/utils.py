5b99fd511325d91c0cb05d9e29aeaf238f128c13
guimas <guimas@ipfn.tecnico.ulisboa.pt>
2017-09-17 14:19:01 +0100

    Some more lessons added. Added functionalitites to my_utils.py.

--- my_utils/utils.py
+++ my_utils/utils.py
@@ -1,13 +1,14 @@
 import numpy as np
 
-def testf(xmin=-5.0, xmax=5.0, dstep=0.01):
-    """Returns a simple sin function to be used in several examples
+def testf(xmin=-5.0, xmax=5.0, dstep=0.01, noise=0.0):
+    """Returns a simple sin function to test several examples
     
     Parameters:
     -----------
     xmin: Lowest x axis value
     xmax: Highest x axis value
     dstep: Step for x-axis
+    noise: Amplitude of the noise to add to the signal, which is a Standard Normal distribution
     
     Returns
     -------
@@ -15,5 +16,20 @@ def testf(xmin=-5.0, xmax=5.0, dstep=0.01):
     y: The sin of x
     """
     x = np.arange(xmin, xmax, dstep)
-    y = np.sin(x)
+    y = np.sin(x) + noise*np.random.randn(len(x))
     return x, y
+
+def mov_avg(x, N=5):
+    """Calculates the moving average through a simple convolution
+    
+    Parameters:
+    -----------
+    x: The data for smoothing
+    N: Number of points used in the smoothing (convulution)
+    
+    Returns
+    -------
+    smt_x: x convolved with a len(N) array where every entry is 1/N    
+    """
+    smt_x = np.convolve(x, np.ones((N,))/N)[(N-1):] 
+    return smt_x
